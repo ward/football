23968c6407129201e51242e2a03857735e657db9
Ward Muylaert <ward.muylaert@gmail.com>
2022-08-06 15:43:09 +0200

    Special case for epl ranking

--- src/ranking/beebs/table.rs
+++ src/ranking/beebs/table.rs
@@ -52,6 +52,20 @@ impl League {
                     vec![]
                 }
             }
+        } else if let Some(json_blob) = Self::find_epl_json_blob(content) {
+            // Very rudimentary cleanup
+            let json_blob = json_blob.replace("\\\"", "\"");
+            log::trace!("cleaned epl_json_blob: {}", json_blob);
+            match serde_json::from_str(&json_blob) {
+                Ok::<ParseEplRanks, _>(parsed) => {
+                    log::trace!("Parsed epl json: {:#?}", parsed);
+                    parsed.to_leagues()
+                }
+                Err(e) => {
+                    eprintln!("Failed to parse: {}", e);
+                    vec![]
+                }
+            }
         } else {
             vec![]
         }
@@ -63,6 +77,16 @@ impl League {
         // Not -1 because the range already excludes this position
         let end_position = content[meta_position..].find(");")? + meta_position;
         Some(&content[meta_position..end_position])
+    }
+
+    /// They got a special table for the EPL which also decides to be completely differently
+    /// implemented...
+    fn find_epl_json_blob(content: &str) -> Option<&str> {
+        let needle_position = content.find("{\\\"participants\\\"")?;
+        log::trace!("needle_position: {}", needle_position);
+        let end_position = content[needle_position..].find("}]}]}]")? + 3 + needle_position;
+        log::trace!("end_position: {}", end_position);
+        Some(&content[needle_position..end_position])
     }
 
     /// Gets all ranked entries
@@ -172,11 +196,7 @@ impl ParseRanks {
             } else {
                 competition_name.to_string()
             };
-            let entries: Vec<Entry> = league
-                .rows
-                .iter()
-                .map(|row| row.to_entry())
-                .collect();
+            let entries: Vec<Entry> = league.rows.iter().map(|row| row.to_entry()).collect();
             leagues.push(League { name, entries })
         }
         leagues
@@ -230,6 +250,51 @@ impl ParseTd {
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct ParseEplRanks {
+    participants: Vec<ParseEplParticipant>,
+}
+#[derive(Deserialize, Debug)]
+struct ParseEplParticipant {
+    rank: i8,
+    name: String,
+    points: i8,
+    wins: i8,
+    losses: i8,
+    draws: i8,
+    #[serde(rename = "goalsScoredFor")]
+    gf: i8,
+    #[serde(rename = "goalsScoredAgainst")]
+    ga: i8,
+}
+impl ParseEplRanks {
+    fn to_leagues(self) -> Vec<League> {
+        let entries = self
+            .participants
+            .into_iter()
+            .map(|part| part.to_entry())
+            .collect();
+        vec![League {
+            name: "English Premier League".to_string(),
+            entries,
+        }]
+    }
+}
+impl ParseEplParticipant {
+    fn to_entry(self) -> Entry {
+        Entry {
+            rank: self.rank,
+            team: self.name,
+            win: self.wins,
+            draw: self.draws,
+            lose: self.losses,
+            gf: self.gf,
+            ga: self.ga,
+            points: self.points,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -256,5 +321,16 @@ mod tests {
         assert_eq!(group_f.name, "Champions League Tables Group F");
 
         assert_eq!(group_f.entries.get(0).unwrap().team, "Manchester United");
+    }
+
+    #[test]
+    fn parse_premier_league() {
+        let _ = env_logger::builder().is_test(true).try_init();
+        let content = include_str!("epl.html");
+
+        let leagues = League::from(content);
+        assert_eq!(leagues.len(), 1);
+        let epl = leagues.get(0).unwrap();
+        assert_eq!(epl.entries.get(2).unwrap().team, "Liverpool");
     }
 }
